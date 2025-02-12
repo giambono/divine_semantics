92352dbe5f679774e73c5fda4c158b639fb2527b
giambono <michele.giambono@gmail.com>
2025-02-12 17:23:44 +0100

    + dante mandatory column

--- examples/run_experiment.py
+++ examples/run_experiment.py
@@ -39,17 +39,3 @@ if __name__ == "__main__":
         # Print the response
         print("Response:", response)
         print()
-
-        """error:
-         "File "/home/rfflpllcn/IdeaProjects/divine_semantics/src/find_similarity.py", line 116, in find_most_similar_ensemble
-         most_similar_verse = df.loc[best_match_idx, "dante"]
-         File "/home/rfflpllcn/IdeaProjects/divine_semantics/venv/lib/python3.9/site-packages/pandas/core/indexing.py", line 1183, in __getitem__
-    return self.obj._get_value(*key, takeable=self._takeable)
-File "/home/rfflpllcn/IdeaProjects/divine_semantics/venv/lib/python3.9/site-packages/pandas/core/frame.py", line 4214, in _get_value
-series = self._get_item_cache(col)
-File "/home/rfflpllcn/IdeaProjects/divine_semantics/venv/lib/python3.9/site-packages/pandas/core/frame.py", line 4638, in _get_item_cache
-loc = self.columns.get_loc(item)
-File "/home/rfflpllcn/IdeaProjects/divine_semantics/venv/lib/python3.9/site-packages/pandas/core/indexes/base.py", line 3812, in get_loc
-raise KeyError(key) from err
-KeyError: 'dante'")
-        """
